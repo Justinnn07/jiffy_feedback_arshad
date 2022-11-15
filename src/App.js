b34a79395ec47de7bcfc7dfa64e4a5caee583cb1
Justin Varghese <justinv2006@gmail.com>
2022-11-15 21:33:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import "./App.css";
 import Hero from "./components/Hero";
 import MainSection from "./components/MainSection";
-import { Container } from "@mui/material";
+import { CircularProgress, Container } from "@mui/material";
 import Footer from "./components/Footer";
 import axios from "axios";
 import config from "./config/config.json";
@@ -16,11 +16,13 @@ const App = () => {
   const [wontShow, setWontShow] = useState(false);
   const [currentData, setCurrentData] = useState("");
   const [showError, setShowError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const url = new URL(window.location);
   const id = url.pathname.split("/");
 
   useEffect(() => {
+    setLoading(true);
     axios.get(config.parcel_api).then((res) => {
       const ids = res.data.parcel.map((res) => res._id);
 
@@ -43,18 +45,22 @@ const App = () => {
 
         axios.get(config.userName_api`?_id=${userId}`).then((res) => {
           setCurrentData(res.data.vendors_collection[0].name);
+          setLoading(false);
+
           // console.log(res.data);
         });
         // [0].pickup[0]
       } else {
         setShowError(true);
         setWontShow(true);
+        setLoading(false);
       }
     });
     //eslint-disable-next-line
   }, []);
 
   const submit = () => {
+    setLoading(true);
     axios.get(config.parcel_api).then((res) => {
       const aParticularData = res.data.parcel.filter(
         (res) => res._id === id[1]
@@ -77,12 +83,16 @@ const App = () => {
             .then((res) => {
               console.log(res.data);
               if (res.data.status === "Success") {
+                setLoading(false);
+
                 alert(res.data.status);
               } else {
                 alert(res.data.ERROR);
               }
             });
         } else {
+          setLoading(false);
+
           alert("Already Submitted");
         }
       } else {
@@ -97,12 +107,15 @@ const App = () => {
             .then((res) => {
               if (res.data.status === "success") {
                 alert(res.data.status);
+                setLoading(false);
               } else {
                 alert(res.data.ERROR);
+                setLoading(false);
               }
             });
         } else {
           alert("Already Submitted");
+          setLoading(false);
         }
       }
     });
@@ -118,30 +131,48 @@ const App = () => {
             "linear-gradient(to right top, #d7ebf5, #d6ecf4, #d5ecf3, #d4edf2, #d4edf1, #d4eeef, #d5efed, #d6f0eb, #daf2e7, #dff3e3, #e7f4e0, #eff4dd)",
         }}
       ></div>
-      {wontShow ? (
-        <Container>
-          <h3>
-            {showError
-              ? "Please Provide proper user / customer id"
-              : "Feedback already submitted"}
-          </h3>
-        </Container>
-      ) : (
-        <Container>
-          <Hero />
-          <MainSection
-            rating={rating}
-            setRating={setRating}
-            setReview={setReview}
-            type={type}
-            setType={setType}
-            review={review}
-            onClick={submit}
-            userName={currentData}
-          />
-          <Footer />
-        </Container>
-      )}
+
+      <>
+        {loading ? (
+          <Box
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "80vh",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            {wontShow ? (
+              <Container>
+                <h3>
+                  {showError
+                    ? "Please Provide proper user / customer id"
+                    : "Feedback already submitted"}
+                </h3>
+              </Container>
+            ) : (
+              <Container>
+                <Hero />
+                <MainSection
+                  rating={rating}
+                  setRating={setRating}
+                  setReview={setReview}
+                  type={type}
+                  setType={setType}
+                  review={review}
+                  onClick={submit}
+                  userName={currentData}
+                />
+                <Footer />
+              </Container>
+            )}
+          </>
+        )}
+      </>
     </Box>
   );
 };
